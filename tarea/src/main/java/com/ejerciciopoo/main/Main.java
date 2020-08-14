aad78514e65d74c1b8fcd057cef929ab6840b719
Angelo <Angelo@DESKTOP-ANVOV73>
2020-08-14 12:57:51 -0500

    taller3

--- tarea/src/main/java/com/ejerciciopoo/main/Main.java
+++ tarea/src/main/java/com/ejerciciopoo/main/Main.java
@@ -1,16 +1,14 @@
 package com.ejerciciopoo.main;
 
-import javax.transaction.Transaction;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
-import com.ejerciciopoo.entidades.Persona;
-import com.ejerciciopoo.entidades.Ranking;
-import com.ejerciciopoo.entidades.Skill;
+import com.ejerciciopoo.entidades.Cliente;
+import com.ejerciciopoo.entidades.Pais;
+
 
 
 
@@ -24,24 +22,19 @@ public class Main {
 	
 
 	public static void main(String[] args) {
-		Persona persona= new Persona("Angelo Silva");
+		Pais pais= new Pais("Turquia");
+		ingresarPais(pais);
+		
+		Cliente persona= new Cliente("Angelo","Silva","Pincay",pais);
 		ingresarPersona(persona);
-		Persona persona1= new Persona("Carlos andrade");
-		ingresarPersona(persona1);
 		
-		Ranking ranking= new Ranking(5);
-		ingresarRanking(ranking);
-		Ranking ranking1= new Ranking(2);
-		ingresarRanking(ranking1);
 		
-		Skill skill= new Skill("Programacion v");
-		ingresarSkill(skill);
-		Skill skil= new Skill("Auditoria 2");
-		ingresarSkill(skil);
+		
+		
 
 		
 	}
-	static void ingresarPersona(Persona persona) {
+	static void ingresarPersona(Cliente persona) {
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
 		session.save(persona);
@@ -49,24 +42,16 @@ public class Main {
 		session.close();
 	}
 	
-	static void ingresarSkill(Skill skill) {
+	static void ingresarPais(Pais pais) {
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
-		session.save(skill);
+		session.save(pais);
 		session.getTransaction().commit();
 		session.close();
 
 	}
 	
-	static void ingresarRanking(Ranking ranking) {
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-		session.save(ranking);
-		session.getTransaction().commit();
-		session.close();
-
-	}
-
+	
 	
 
 
